6ec40bc032f38a00c7ccb825fa75a81b4c24dd95
psnoya <paulina.s.noya@gmail.com>
2020-12-15 00:02:47 +0100

    hello world from state in component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const App = () => {
+  // let message = "Hello World from variable"
+  const [message, setMessage ] = useState('Hello World from hooked state')
+  return (
+    <>
+      <h1>{message}</h1>
+    </>
+  )
+}
+
+export default App
